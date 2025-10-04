e9cb05c5663f1e61e765ee873778623d59b70407
Rafaellc-DEV <rafaelcosta.2600@gmail.com>
2025-10-03 22:03:54 -0300

    Criando a estrutura do projeto

---
+++ src/main/java/br/com/projetos3/visita/VisitaBrennandApplication.java
@@ -0,0 +1,13 @@
+package br.com.projetos3.visita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisitaBrennandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisitaBrennandApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projetos3/visita/VisitaBrennandApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projetos3.visita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisitaBrennandApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
